bd3fe13091aa1d5098ad5c7eb31cd5e7f4b0ecee
Syarah11 <aisysyarahh@gmail.com>
2024-09-28 00:34:39 +0700

    Add Bunga Tulip

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,21 @@ public class Main {
         System.out.println("Iklim             : " + fr.getIklim());
         System.out.println("Tanah             : " + fr.getTanah());
         System.out.println("Hewan Penyerbuk   : " + fr.getHewan_penyerbuk());
+        
+        
+        Bunga_Tulip st = new Bunga_Tulip();
+
+        st.setnama_ilmiah("Tulipa");
+        st.setWarna("Beragam(Merah, Kuning, Putih, Ungu)");
+        st.setIklim("Sedang");
+        st.setTanah("Berdrainase baik dan subur");
+        st.setHewan_penyerbuk("Lebah");
+
+        System.out.println("\n\t\t\tBunga Tulip");
+        System.out.println("Nama Ilmiah       : " + st.getnama_ilmiah());
+        System.out.println("Warna             : " + st.getWarna());
+        System.out.println("Iklim             : " + st.getIklim());
+        System.out.println("Tanah             : " + st.getTanah());
+        System.out.println("Hewan Penyerbuk   : " + st.getHewan_penyerbuk());
 }
 }
