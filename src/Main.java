f9b8dc47a441ec2f76cc62f760f4b51cb5fc445b
dickyi <irawandicky300@gmail.com>
2024-09-28 00:18:45 +0700

    isi coding bunga dan main-nya ok

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,20 @@ public class Main {
         System.out.println("Iklim             : " + fr.getIklim());
         System.out.println("Tanah             : " + fr.getTanah());
         System.out.println("Hewan Penyerbuk   : " + fr.getHewan_penyerbuk());
+        
+         Bunga_Kamboja bunga = new Bunga_Kamboja();
+
+        bunga.setnama_ilmiah("Plumeria rubra");
+        bunga.setWarna("Merah");
+        bunga.setIklim("Tropis");
+        bunga.setTanah("Berdrainase baik");
+        bunga.setHewan_penyerbuk("Lebah");
+
+        System.out.println("\t\t\tBunga Kamboja");
+        System.out.println("Nama Ilmiah       : " + bunga.getnama_ilmiah());
+        System.out.println("Warna             : " + bunga.getWarna());
+        System.out.println("Iklim             : " + bunga.getIklim());
+        System.out.println("Tanah             : " + bunga.getTanah());
+        System.out.println("Hewan Penyerbuk   : " + bunga.getHewan_penyerbuk());
 }
 }
